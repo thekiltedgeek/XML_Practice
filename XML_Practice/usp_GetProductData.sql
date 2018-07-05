6af2219e633db631b60e00817a32c7ab580bf3c6
Larry Shanahan <lshanahan@srisaibiopharma.loc>
2018-07-05 14:33:47 -0400

    Added usp_GetProductsData.sql

---
+++ XML_Practice/usp_GetProductData.sql
@@ -0,0 +1,45 @@
+use XML_Practice;
+go
+
+drop procedure if exists dbo.usp_GetProductData;
+go
+
+create procedure dbo.usp_GetProductData
+as
+begin
+
+declare
+	@columns nvarchar(max)
+	,@sql nvarchar(max)
+	;
+
+set @columns = '';
+
+select @columns += N', ' + quotename(colname)
+from
+	(
+		select
+			f.d.value('.','nvarchar(128)') as colname
+		from dbo.FlexDefinitions as fsd cross apply fsd.setcolumns.nodes('/flexsetdef/column') as f(d)
+	) as cols;
+
+set @sql = N'
+with flexpivot
+as
+(
+	select
+		p.productid
+		,p.productname
+		,f.d.value(''./@name'',''nvarchar(128)'') as colname
+		,f.d.value(''.'',''nvarchar(100)'') as coldata
+	from dbo.Products as p cross apply p.productflexdata.nodes(''/flexset/flexfield'') as f(d)
+	where p.productflexdata is not null
+)
+
+select productid, productname' + @columns +
+'from flexpivot
+	pivot (max(coldata) for colname in (' + stuff(@columns,1,2,'') + ')) as fp;'
+
+exec sp_executesql @stmt = @sql;
+
+end
